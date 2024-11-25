a4ef47afbfbd7300d441a69632878cd50ceca2e4
Grimpeach <grig02092003@gmail.com>
2024-11-25 20:49:32 +0300

    bazed com 1

--- app.js
+++ app.js
@@ -7,6 +7,19 @@ const mongooseConfig = require('./lib/mongoose-config')
 
 const app = new Koa()
 
+const path = require('path');
+const serve = require('koa-static');
+
+app.use(serve(path.join(__dirname, 'client/build')));
+
+app.use(async (ctx, next) => {
+  if (ctx.status === 404) {
+    ctx.body = require('fs').readFileSync(path.join(__dirname, 'client/build/index.html'), 'utf-8');
+  } else {
+    await next();
+  }
+});
+
 handlers.forEach((h) => app.use(h))
 
 app.use(controllers.routes())
